beae031fcc39a1268721cc76f9d7211ba74cb3c3
shadow <darkshadowblaze786@g.mail.com>
2025-08-04 11:49:44 +0600

    role

--- Per-Ankh/migrations/20250804050532_projet_user_table.js
+++ Per-Ankh/migrations/20250804050532_projet_user_table.js
@@ -1,7 +1,8 @@
 exports.up = function (knex) {
   return knex.schema.createTable("project_user", function (table) {
-    table.increment("pu_id");
+    table.increments("pu_id");
     table.integer("user_id").references("use_id").inTable("user");
+    table.string("role").notNullable();
     table.integer("project_id").references("project_id").inTable("project");
   });
 };
