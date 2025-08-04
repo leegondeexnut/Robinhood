0e070c54a65c9ab834689bb2fb13d8be6ba27669
shadow <darkshadowblaze786@g.mail.com>
2025-08-04 15:13:22 +0600

    api reuests

--- Per-Ankh/app/api/user/route.tsx
+++ Per-Ankh/app/api/user/route.tsx
@@ -4,10 +4,17 @@ require("dotenv").config();
 const kn = knex(knexConfig.development);
 
 export async function GET() {
-  const users = await kn("user").select("*");
+  const users = await kn("users").select("*");
   return Response.json({ users });
 }
 
 export async function POST(request: Request) {
-  const { name };
+  const { username, email, bio, password, role } = await request.json();
+  const [createdfriend] = await kn("users")
+    .insert({ username, email, bio, password, role })
+    .returning("*");
+  return Response.json(
+    { message: "user created", user: createdfriend.detail },
+    { status: 201 }
+  );
 }
--- Per-Ankh/migrations/20250801072953_usertable.js
+++ Per-Ankh/migrations/20250801072953_usertable.js
@@ -2,23 +2,24 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-    return knex.schema.createTable('users', function (table){
-        table.increments('use_id')
-        table.string('username', 104).notNullable()
-        table.string('email', 254).notNullable()
-        table.string('bio', 512).nullable().defaultTo('')
-        table.string('password', 512).notNullable()
-        table.string("image_url", 512).nullable().defaultTo('https://img.freepik.com/premium-vector/parrot-head-mascot-cartoon-vector-illustration-isolated-white-background_1151-78624.jpg')
-        table.string('role', 64).notNullable()
-        table.timestamps(true, true)
-
-
-    })
-  
+exports.up = function (knex) {
+  return knex.schema.createTable("users", function (table) {
+    table.increments("use_id");
+    table.string("username", 104).notNullable();
+    table.string("email", 254).notNullable();
+    table.string("bio", 512).nullable().defaultTo("");
+    table.string("password", 512).notNullable();
+    table
+      .string("image_url", 512)
+      .nullable()
+      .defaultTo(
+        "https://img.freepik.com/premium-vector/parrot-head-mascot-cartoon-vector-illustration-isolated-white-background_1151-78624.jpg"
+      );
+    table.string("role", 64).notNullable();
+    table.timestamps(true, true);
+  });
 };
 
-exports.down = function(knex) {
-    return knex.schema.dropTable('users')
-  
+exports.down = function (knex) {
+  return knex.schema.dropTable("users");
 };
