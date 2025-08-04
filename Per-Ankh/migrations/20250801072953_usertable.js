6e8931619eff116f72dfb5e47917a5791a0a215a
leegondeexnut <trekkreck@gmail.com>
2025-08-04 10:44:40 +0600

    ayaya mhmhmhhh hmmm tsm tsm tsm

--- Per-Ankh/migrations/20250801072953_usertable.js
+++ Per-Ankh/migrations/20250801072953_usertable.js
@@ -3,13 +3,22 @@
  * @returns { Promise<void> }
  */
 exports.up = function(knex) {
+    knex.schema.createTable('user', function (table){
+        table.increments('id')
+        table.string('username', 104).notNullable()
+        table.string('email', 254).notNullable()
+        table.string('bio', 512).nullable().defaultTo('')
+        table.string('password', 512).notNullable()
+        table.string("image_url", 512).nullable().defaultTo('https://img.freepik.com/premium-vector/parrot-head-mascot-cartoon-vector-illustration-isolated-white-background_1151-78624.jpg')
+        table.string('role', 64).notNullable()
+        table.timestamps(true, true)
+
+
+    })
   
 };
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
 exports.down = function(knex) {
+    return knex.schema.dropTable('users')
   
 };
