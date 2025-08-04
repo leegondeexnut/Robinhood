8d09bfb0bfe18ac39d6bf6db05bf38a31c805755
leegondeexnut <trekkreck@gmail.com>
2025-08-04 09:41:38 +0600

    ohh now our project starts

--- Per-Ankh/app/components/Nav.tsx
+++ Per-Ankh/app/components/Nav.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 export default function Nav({ title, page }: { title: string; page: { href: string; title: string }[] }) {
     return (
-        <nav className="bg-gray-500/80 text white w-full py-2 px-8 flex flex-cols justify-between fixed">
+        <nav className=" border-2 border-[#3d3d3d] rounded-lg text white w-[95%] mx-8 m-4 py-2 px-8 flex flex-cols justify-between fixed">
             <div><h2 className="text-xl py-4">{title}</h2></div>
             <ul>
                 {
