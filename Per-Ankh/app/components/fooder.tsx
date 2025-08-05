bb10fdb9545bbdd0c8a6adf8414e31cafba4908c
shadow <darkshadowblaze786@g.mail.com>
2025-08-05 10:44:30 +0600

    footer 2.0

--- Per-Ankh/app/components/fooder.tsx
+++ Per-Ankh/app/components/fooder.tsx
@@ -1,10 +1,14 @@
 "use client";
 
 import Link from "next/link";
+import { FaFacebook } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa6";
+import { FaWhatsapp } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
 
 export default function Fooder() {
   return (
-    <footer className="bg-gray-600 text-gray-300 px-6 py-10 mt-10">
+    <footer className="bg-gray-600 text-gray-300 px-6 py-10 mt-10 w-full absolute bottom-0">
       <div className="max-w-7xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8">
         <div>
           <h2 className="text-xl font-bold text-white">MyApp</h2>
@@ -56,13 +60,21 @@ export default function Fooder() {
           </form>
         </div>
 
-        <div>
-          <h3 className="text-white font-semibold mb-2">Follow Us</h3>
+        <div className="ml-30">
+          <h3 className="text-white font-semibold mb-2 ml-12">Follow Us</h3>
           <div className="flex space-x-4 mt-2 text-xl">
-            <Link href="https://facebook.com" target="_blank"></Link>
-            <Link href="https://twitter.com" target="_blank"></Link>
-            <Link href="https://instagram.com" target="_blank"></Link>
-            <Link href="https://github.com" target="_blank"></Link>
+            <Link href="https://facebook.com" target="_blank">
+              <FaFacebook className="w-8 h-8 hover:bg-red-300 hover:animate-bounce cursor-pointer"></FaFacebook>
+            </Link>
+            <Link href="https://twitter.com" target="_blank">
+              <FaTwitter className="w-8 h-8 hover:bg-red-300 hover:animate-bounce cursor-pointer"></FaTwitter>
+            </Link>
+            <Link href="https://instagram.com" target="_blank">
+              <FaInstagram className="w-8 h-8 hover:bg-red-300 hover:animate-bounce cursor-pointer"></FaInstagram>
+            </Link>
+            <Link href="https://whatsapp.com" target="_blank">
+              <FaWhatsapp className="w-8 h-8 hover:bg-red-300 hover:animate-bounce cursor-pointer"></FaWhatsapp>
+            </Link>
           </div>
         </div>
       </div>
