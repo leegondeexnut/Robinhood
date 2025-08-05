60742c2c4fa353a2a4c9d0dc1483488767169cb7
leegondeexnut <trekkreck@gmail.com>
2025-08-05 13:01:07 +0600

    nav bar official

--- Per-Ankh/app/components/Nav.tsx
+++ Per-Ankh/app/components/Nav.tsx
@@ -1,31 +1,31 @@
 'use client'
 import Link from 'next/link';
 
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 
 
 export default function Nav({ title, page }: { title: string; page: { href: string; title: string }[] }) {
 
         const [ scrollDown, setScrollDown] = useState(false)
-        const [ lastY, setLastY] = useState(0)
+        const lastYRef = useRef(0);
         useEffect(()=>{
             function onScroll(){
                 const currentY = window.scrollY;
-                if (currentY > lastY){
+                if (currentY > lastYRef.current){
                     setScrollDown(true)
                 } else{
                     setScrollDown(false)
                 }
-                setLastY(currentY)
+                lastYRef.current= currentY;
             }
             window.addEventListener('scroll', onScroll);
             return () => window.removeEventListener('scroll', onScroll);
 
-        }, [lastY])
+        }, [])
         
     return (
         
-        <nav className={`border-2 border-[#3d3d3d] rounded-lg text white w-[95%] mx-8 m-4 py-2 px-8 flex flex-cols justify-between fixed transition-transform duration-300 ${scrollDown ? 'transtlate-y-full opacity-0': 'translate-y-0 opacity-100'}`}>
+        <nav className={`border-2 border-[#3d3d3d] rounded-lg text-white w-[95%] mx-8 m-4 py-2 px-8 flex flex-cols justify-between fixed transition-transform duration-300 ${scrollDown ? 'translate-y-full hidden': 'translate-y-0 opacity-100'}`}>
             <div><h2 className="text-xl py-4">{title}</h2></div>
             <ul>
                 {
