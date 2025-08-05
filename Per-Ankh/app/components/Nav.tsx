b30a4054fdd3eb51604ef45cc17fa842ca5c3d08
leegondeexnut <trekkreck@gmail.com>
2025-08-05 11:49:26 +0600

    i added nav but seems like deleted some

--- Per-Ankh/app/components/Nav.tsx
+++ Per-Ankh/app/components/Nav.tsx
@@ -1,16 +1,32 @@
+'use client'
 import Link from 'next/link';
-export default function Nav({ title, page }: { title: string; page: { href: string; title: string }[] }) {
-    return (
-        <nav className=" border-2 border-[#3d3d3d] rounded-lg text white w-[95%] mx-8 m-4 py-2 px-8 flex flex-cols justify-between fixed">
-            <div><h2 className="text-xl py-4">{title}</h2></div>
-            <div className="flex items-center gap-4">
-                <input
-                    type="text"
-                    placeholder="Search for anything"
-                    className="px-10 py-2 rounded-md text-lg text-white"/>
-            <button className="flex items-center gap-4 rounded-md text-lg text-white bg-blue-500 cursor-pointer px-2 py-2">Search</button>
-             </div>
 
+import { useEffect, useState } from 'react';
+
+
+export default function Nav({ title, page }: { title: string; page: { href: string; title: string }[] }) {
+
+        const [ scrollDown, setScrollDown] = useState(false)
+        const [ lastY, setLastY] = useState(0)
+        useEffect(()=>{
+            function onScroll(){
+                const currentY = window.scrollY;
+                if (currentY > lastY){
+                    setScrollDown(true)
+                } else{
+                    setScrollDown(false)
+                }
+                setLastY(currentY)
+            }
+            window.addEventListener('scroll', onScroll);
+            return () => window.removeEventListener('scroll', onScroll);
+
+        }, [lastY])
+        
+    return (
+        
+        <nav className={`border-2 border-[#3d3d3d] rounded-lg text white w-[95%] mx-8 m-4 py-2 px-8 flex flex-cols justify-between fixed transition-transform duration-300 ${scrollDown ? 'transtlate-y-full opacity-0': 'translate-y-0 opacity-100'}`}>
+            <div><h2 className="text-xl py-4">{title}</h2></div>
             <ul>
                 {
                     page.map((item)=> (
@@ -18,12 +34,10 @@ export default function Nav({ title, page }: { title: string; page: { href: stri
                         <Link href={item.href}>
                         {item.title}
                         </Link>
-                        
                         </li>
                     ))
                 }
             </ul>
-            
         </nav>
     )
 }
