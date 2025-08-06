f39360efb39a792df4446f07b7e904f0f7967b7a
shadow <darkshadowblaze786@g.mail.com>
2025-08-06 15:18:27 +0600

    bio details function

--- Per-Ankh/app/components/Search.tsx
+++ Per-Ankh/app/components/Search.tsx
@@ -1,13 +1,23 @@
-'use client'
+"use client";
 
-
-
-export default function Search({ Id, Placeholder }:{Id: string; Placeholder: string}){
-    return(
+export default function Search({
+  Id,
+  Placeholder,
+}: {
+  Id: string;
+  Placeholder: string;
+}) {
+  return (
     <div className="flex h-12">
-        <input id={Id} name={Id} placeholder={Placeholder} className=" border-2 border-gray-600 p-2 rounded"></input>
-        <button className="bg-gray-600 rounded m-2 p-2">Go</button>
-        
+      <input
+        id={Id}
+        name={Id}
+        placeholder={Placeholder}
+        className=" border-2 border-gray-600 p-2 rounded-2xl border-white"
+      ></input>
+      <button className="bg-red-300 rounded-lg w-10 h-10 ml-2 mt-1 p-2 hover:bg-blue-300 cursor-pointer">
+        Go
+      </button>
     </div>
-    )
+  );
 }
